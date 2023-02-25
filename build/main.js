95f371f3bd678e58c31e3971f59eb07912c791e6
Fares Hassen <fares.hassen512@gmail.com>
2023-02-25 01:51:27 +0100

    test

--- build/main.js
+++ build/main.js
@@ -214,7 +214,7 @@ setInterval(() => {
         soon.style.color = "white";
         soon.style.textShadow = "";
     }, 800);
-}, 2000);
+}, 4500);
 document.body.onpointermove = event => {
     const { clientX, clientY } = event;
     blob.animate({
